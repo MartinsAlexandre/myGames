2a7caa367ba10ee2e73198afd6560079f936c66b
Alexandre <alexandre.martins@osmose-solutions.com>
2022-06-05 19:10:49 +0200

    install url-loader for fix error import imgs

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,15 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/myGames/'
-  : '/',
-  transpileDependencies: [
-    'vuetify'
-  ]
-}
+  publicPath: process.env.NODE_ENV === "production" ? "/myGames/" : "/",
+  transpileDependencies: ["vuetify"],
+
+  chainWebpack: (config) => {
+    config.module
+      .rule("images")
+      .test(/\.(png|jpg|gif)$/i)
+      .use("url-loader")
+      .loader("url-loader")
+      .options({
+        limit: false
+      })
+  }
+};
